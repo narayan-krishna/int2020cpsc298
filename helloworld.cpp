4f7edb6dbbee70862052d05e5ed20a9042dcd0ef
narayan-krishna <narayan@chapman.edu>
2020-01-14 20:54:00 +0000

    small changes

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(int argc, char **argv)
 {
   cout << "hello world" << endl;
+  cout << "I had to add this statement because rene said so" << endl;
+  cout << "adding another cout" << endl;
 
   return 0;
 }
