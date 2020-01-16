4e39b77aeec628a6bdf94591ef0b802c1c6fea20
narayan-krishna <narayan@chapman.edu>
2020-01-16 18:42:26 +0000

    problem2 and the start of problem3

---
+++ Problem2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+// Write a program that determines whether a meeting room is in violation of fire law regula- tions regarding
+// the maximum room capacity. The program will read in the maximum room capacity and the number of
+// people to attend the meeting. If the number of people is less than or equal to the maximum room capacity,
+// the program announces that it is legal to hold the meeting and tells how many additional people may
+// legally attend. If the number of people exceeds the maximum room capacity, the program announces that
+// the meeting cannot be held as planned due to fire regulations and tells how many people must be excluded
+// in order to meet the fire regulations.
+
+int main(int argc, char **argv)
+{
+  int maximumRoomCapacity = 0;
+  int meetingAttendance = 0;
+
+  cout << "enter the maximum capacity of the room: ";
+  cin >> maximumRoomCapacity;
+
+  cout << "enter the number of people attending the meeting: ";
+  cin >> meetingAttendance;
+
+  if(meetingAttendance <= maximumRoomCapacity)
+  {
+    cout << "it is legal to hold the meeting. ";
+    cout << maximumRoomCapacity - meetingAttendance;
+    cout << " more people can attend the meeting before hitting max capacity." << endl;
+  }
+  else
+  {
+    cout << "the meeting cannot be held as planned due to fire regulations. ";
+    cout << meetingAttendance - maximumRoomCapacity;
+    cout << " people must be excluded in order to meet fire regulations." << endl;
+  }
+}
---
+++ Problem3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// An employee is paid at a rate of $16.00 per hour for regular hours worked in a week. Any hours over that
+// are paid at the overtime rate of one and one-half times that. From the worker’s gross pay, 6% is withheld
+// for Social Security tax, 14% is withheld for federal income tax, 5% is withheld for state income tax, and
+// $10 per week is withheld for medical insurance. Write a program that will read in the number of hours
+// worked in a week as input and that will then output the worker’s gross pay, each withholding amount, and
+// the net take-home pay for the week.
+
+int main(int argc, char **argv)
+{
+  
+}
