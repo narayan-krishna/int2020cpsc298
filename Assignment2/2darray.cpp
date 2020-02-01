e53ec4aaf752d5d4cc296e7bf32a6e791b5e6685
narayan-krishna <narayan@chapman.edu>
2020-02-01 06:27:00 +0000

    comments on 2d array

--- Assignment2/2darray.cpp
+++ Assignment2/2darray.cpp
@@ -3,22 +3,24 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-  int a[4][5];
+int a[4][5]; //initialize array
 
-  for(int i = 0; i < 4; ++i){
-    char numberString[5];
-    cout << "input 5 numbers: "; cin >> numberString;
-    for(int j = 0; j < 5; ++j){
-      a[i][j] = (int)numberString[j] - 48;
-    }
+//for loop runs through first dimension of array
+for(int i = 0; i < 4; ++i){
+  char numberString[5]; //initialize character string for user input
+  cout << "input 5 numbers: "; cin >> numberString; //take in input
+  for(int j = 0; j < 5; ++j){ //run through second dimension of array
+    a[i][j] = (int)numberString[j] - 48; //translate char to int, fill array
   }
+}
 
-  for(int x = 0; x < 4; ++x){
-    for(int y = 0; y < 5; ++y){
-      cout << a[x][y];
-    }
-    cout << endl;
+//print array back out for testing purposes.
+for(int x = 0; x < 4; ++x){
+  for(int y = 0; y < 5; ++y){
+    cout << a[x][y];
   }
+  cout << endl;
+}
 
-  return 0;
+return 0;
 }
